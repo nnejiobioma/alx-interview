080680881787a7b98d20c4df64337ff29537a465
nnejiobioma <nnejiobioma@gmail.com>
2023-10-26 19:05:43 +0000

    made changes to code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,36 +4,31 @@ represents a valid UTF-8 encoding.
 """
 
 
-def get_leading_set_bits(num):
-    """Return:
-True if data is a valid UTF-8 encoding,
-else return False
-"""
-    bits = 0
-    helper = 1 << 7
-    while helper & num:
-        bits += 1
-        helper = helper >> 1
-    return bits
-
-
 def validUTF8(data):
-    """ This is set to gets if a given data set
-represents a valid UTF-8 encoding
-"""
-    count = 0
-    for i in range(len(data)):
-        if count == 0:
-            count = get_leading_bits(data[i])
-            '''1-byte (format: 0xxxxxxx)'''
-            if count == 0:
-                continue
-            '''UTF-8 character this can be 1 to 4 bytes long'''
-            if count == 1 or count > 4:
+    # Initialize a variable to keep track of the number of bytes expected
+    bytes_to_follow = 0
+
+    for byte in data:
+        # Check if the byte is a continuation byte (has the format 10xxxxxx)
+        if 0b10000000 <= byte <= 0b10111111:
+            # If it's not a valid continuation byte, return False
+            if bytes_to_follow == 0:
                 return False
+            bytes_to_follow -= 1
         else:
-            '''checks current byte format 10xxxxxx'''
-            if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
+            if bytes_to_follow > 0:
                 return False
-        count -= 1
-    return count == 0
+            # Determine the number of bytes to follow based on the first few bits of the byte
+            if byte & 0b10000000 == 0:
+                bytes_to_follow = 0
+            elif byte & 0b11100000 == 0b11000000:
+                bytes_to_follow = 1
+            elif byte & 0b11110000 == 0b11100000:
+                bytes_to_follow = 2
+            elif byte & 0b11111000 == 0b11110000:
+                bytes_to_follow = 3
+            else:
+                return False
+
+    # If there are still bytes to follow, it's an incomplete sequence
+    return bytes_to_follow == 0
