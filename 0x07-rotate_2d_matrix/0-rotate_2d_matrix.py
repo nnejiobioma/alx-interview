07dbf49eb49ed62e3dc76b645a82c9f50500cf27
nnejiobioma <nnejiobioma@gmail.com>
2023-11-13 17:55:09 +0000

    task one handled

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This rotate 2D matrix module.
+"""
+def rotate_2d_matrix(matrix):
+    """Rotate it 90 degrees clockwise.
+    """
+
+    c = matrix.copy()
+
+    for iter in range(len(matrix[0])):
+        aux = []
+        for row in reversed(c):
+            aux.append(row[iter])
+        matrix[iter] = aux
